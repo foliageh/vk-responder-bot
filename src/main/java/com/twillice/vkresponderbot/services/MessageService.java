12bd4fdda224def4c0f38bd025ec9c88692bb734
foliageh <mygazztop1@gmail.com>
2024-05-28 17:38:16 +0300

    improve random_id generation

--- src/main/java/com/twillice/vkresponderbot/services/MessageService.java
+++ src/main/java/com/twillice/vkresponderbot/services/MessageService.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.Map;
-import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
     private final RestClient apiClient;
 
-    private final Random random = new Random();
+    private final AtomicInteger messageCounter = new AtomicInteger();
 
     public void replyNewMessage(NewMessage newMessage) {
         Message userMessage = newMessage.getMessage();
@@ -27,10 +27,14 @@ public class MessageService {
                 .uri(uriBuilder -> uriBuilder
                         .path("/messages.send")
                         .queryParam("user_id", userId)
-                        .queryParam("random_id", random.nextInt())
+                        .queryParam("random_id", generateUniqueRandomId())
                         .queryParam("message", text)
                         .build())
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {});
+    }
+
+    private int generateUniqueRandomId() {
+        return Integer.MIN_VALUE + (int) (System.currentTimeMillis() / 1000) + messageCounter.getAndIncrement();
     }
 }
